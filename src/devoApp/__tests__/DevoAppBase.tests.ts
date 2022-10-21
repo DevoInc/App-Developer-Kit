ee6364abfc6d84a3bea81191540b9742b9234ea2
carlos.delacuadra <carlos.delacuadra@devo.com>
2022-10-21 10:26:33 +0200

    chore: lint format

--- src/devoApp/__tests__/DevoAppBase.tests.ts
+++ src/devoApp/__tests__/DevoAppBase.tests.ts
@@ -31,7 +31,8 @@ class DevoAppTest extends DevoAppBase implements IDevoApp {
   createNotiPop(_request: NotiPopRequest): Promise<void> {
     return new Promise<void>((resolve) => resolve());
   }
-  setAppUnmountCallback(_cb: () => void): void {}
+
+  setAppUnmountCallback(_cb: () => void): void {} // eslint-disable-line @typescript-eslint/no-empty-function
 }
 
 const mockedUserInfo: UserInfo = {
