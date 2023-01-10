80bf317c80fd2922a9f6a0305ef900c55f43243e
carlos.delacuadra <carlos.delacuadra@devo.com>
2023-01-10 12:44:52 +0100

    chore: better typing of userCredentials

--- src/types/UserCredentials.ts
+++ src/types/UserCredentials.ts
@@ -3,25 +3,67 @@
  *
  * @public
  */
-export type UserCredentials = {
-  readonly alertsURI: string;
-  readonly apiKey: string;
-  readonly apiSecret: string;
-  readonly enigmaURI: string;
-  readonly investigationsURI: string;
-  readonly jwtSessionCredentials: {
-    readonly apiKey: string;
-    readonly jwtUserId: string;
-  };
-  readonly marketplaceBundle: string;
-  readonly marketplaceURI: string;
-  readonly activeboardsURI: string;
-  /**
-   * Serrea url
-   */
-  readonly serrea: string;
+export interface UserCredentials extends Readonly<Record<string, any>> {
+
+  // Credentials for the current user
+
   /**
    * Tapu token
    */
-  readonly standAloneToken: string;
+  standAloneToken: string;
+
+  /**
+   * Devo api key
+   */
+  apiKey: string;
+
+  /**
+   * Devo api secret
+   */
+  apiSecret: string;
+
+  /**
+   * JWT Session credentials
+   */
+  jwtSessionCredentials: {
+    readonly apiKey: string;
+    readonly jwtUserId: string;
+  };
+
+  // API urls
+
+  /**
+   * Alerts API url
+   */
+  alertsURI: string;
+
+  /**
+   * Enigma API url
+   */
+  enigmaURI: string;
+
+  /**
+   * Investigations API url
+   */
+  investigationsURI: string;
+
+  /**
+   * Marketplace API url
+   */
+  marketplaceURI: string;
+
+  /**
+   * Activeboards API url
+   */
+  activeboardsURI: string;
+
+  /**
+   * Service operations API url
+   */
+  serviceopsURI: string;
+
+  /**
+   * Serrea API url
+   */
+  serrea: string;
 };
